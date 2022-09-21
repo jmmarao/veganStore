9489420fa460b46b619eb38a2321fa86d2c0b5d0
jmmarao <jmmarao@gmail.com>
2022-09-21 10:00:44 -0300

    Project initialize

---
+++ src/main/java/br/edu/ifsp/veganStore/VeganStoreApplication.java
@@ -0,0 +1,13 @@
+package br.edu.ifsp.veganStore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VeganStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VeganStoreApplication.class, args);
+	}
+
+}
