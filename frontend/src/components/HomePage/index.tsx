c3716eea18a643196b9ea726faa937267c38ab65
Roberto Molina <rasmolina@MacBook-Air-de-Roberto.local>
2022-11-11 19:57:27 -0300

    bar menu

--- frontend/src/components/HomePage/index.tsx
+++ frontend/src/components/HomePage/index.tsx
@@ -1,9 +1,10 @@
 import { Link } from "react-router-dom";
+import './styles.css';
 
 function HomePage() {
     return (
-        <div>
-            <h1>Home Page</h1>
+        <div className="menubar">
+            <h1>Seja Bem Vindo à Vegan Store</h1>
             
             <Link to="/customer"><button className="btn">Clientes</button></Link>
             <Link to="/product"><button className="btn">Produtos</button></Link>
